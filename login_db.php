2876538208b80fc3b5f6ec50c7a2f4a1608c3aa2
keeta <keeta.kam@outlook.com>
2025-01-16 10:59:31 +0700

    commit

--- login_db.php
+++ login_db.php
@@ -1,56 +1,27 @@
 <?php
-include("connect.php");
-
 session_start();
 if(isset($_POST['username']) && isset($_POST['password'])){
-    // function validate($data){
-    //     $data = trim($data);
-    //     $data = stripcslashes($data);
-    //     $data = htmlspecialchars($data);
-    //     return $data;
-
-     echo 'get data from login'; 
-    //  echo $Username. $Password;
+    function validate($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+}
 }
 
-$User = $_POST['username'];
-$Pass = $_POST['password'];
+$uname = validate($_POST['username']);
+$pass = validate($_POST['password']);
+
+echo $uname;
+echo $pass;
+
+$_SESSION['username'] = $uname;
+$_SESSION['password'] = $pass;
+
+echo $_SESSION['username'];
 
 
-echo "data User is $User";
-echo "data password is $Pass";
-
-echo "Your can see this message therefore  your User or password is not True "
-
+header("Location: index.html" );
+    exit();
 ?>
 
-<?php
-    $q = "select * from users where username = '$User' AND password = '$Pass' ";
-    $result = sqlsrv_query($conn,$q);
-?>
-
-
-<?php 
-    $num = 0;
-    while($row = sqlsrv_fetch_array($result,SQLSRV_FETCH_ASSOC))
-    {
-?>
-        <br>
-        <?php echo $num+=1; ?>
-        username
-        <?php echo $row['username']; ?>
-        <br>
-        password
-        <?php echo $row['password']; ?>
-        <br>
-        role
-        <?php echo $row['role'];?>
-
-        
-   <?php      
-       header("Location: index.html" );
-       exit();
-    }
-    // header("Location: index.html" );
-    // exit();
-    ?>
